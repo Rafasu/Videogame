308cd97533216c6dd106826f259853a21ae77773
Rafael Solis <a01172744@itesm.mx>
2018-04-03 15:20:27 -0600

    Modified obstaculos

--- prototipo2/src/ClaseObstaculo.java
+++ prototipo2/src/ClaseObstaculo.java
@@ -1,20 +1,47 @@
 import java.awt.Color;
 import java.awt.Graphics;
 
-public class ClaseObstaculo {
+    public class ClaseObstaculo {
 
-	static int y = 10;
-	static int x = ((int)(Math.random() * 350) + 1);;
-	
-	
-	public void paint(Graphics g){
+        //Dimensiones obstaculo
+        public static final int WIDTH = 30;
+        public static final int HEIGHT = 30;
 
-		g.setColor(Color.blue);
-		g.fillRect(x, y, 30, 30);
-	}
+       /*Posición inicial del obstaculo. 
+            Colocar obstaculos al inicio en y. 
+            Posición random en x.
+        */
+        int y = 10;
+        int x = ((int)(Math.random() * 350) + 1);
 
-	public void CalcularPosY()
-	{
-		y += 1;
-	}
-}
+        public ClaseObstaculo(int x, int y){
+            this.x = x;
+            this.y = y;
+
+        }
+
+        public ClaseObstaculo move(int dx, int dy)
+        {
+            return new ClaseObstaculo(x+dx, y+dy);
+        }
+
+
+        public Point getLocation()
+        {
+           return new Point(x,y);
+        }
+
+        // Dibujar al obstaculo
+        public void paint(Graphics g){
+
+                g.setColor(Color.blue);
+                g.fillRect(x, y, HEIGHT, WIDTH);
+        }
+
+
+        //Mover Obstaculo
+        public void CalcularPosY()
+        {
+                y += 1;
+        }
+    }
